410a058effc5eb67ddcce5f208ec3d0d4c730a14
fxcx <87233669+fxcx@users.noreply.github.com>
2023-10-11 20:46:20 -0300

    add json

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-
 const express = require("express");
 
 
 const app = express();
 
 
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+app.get("/data", (req, res) => {
+  // Leer todos los datos desde tu archivo JSON y responder con ellos
+  const data = require("./db.json");
+  res.json(data);
 });
-
 
 app.listen(5000, () => {
   console.log("Running on port 5000.");
