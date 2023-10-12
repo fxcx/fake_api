cefcc19fa1589734b67bc80bf60e26b186da3614
fxcx <87233669+fxcx@users.noreply.github.com>
2023-10-11 21:08:02 -0300

    agregue mas datos a la fake_api

---
+++ db..js
@@ -0,0 +1,98 @@
+export const FAKE_DB = {
+   "acompañamiento": [
+        {
+            "id": 1,
+            "name": "Papas Fritas",
+            "price": 1000,
+            "description": "Papas Fritas",
+            "image": "https://res.cloudinary.com/dbry8g6vk/image/upload/v1695230213/papas_fritas_5fc665a728.png"
+        },
+        {
+            "id": 2,
+            "name": "Papas a la provenzal",
+            "price": 1000,
+            "description": "Papas fritas con ajo y perejil",
+            "image": "https://res.cloudinary.com/dbry8g6vk/image/upload/v1695230288/papas_provenzal_76e0acb615.png"
+        },
+        {
+            "id": 3,
+            "name": "Pure de papas",
+            "price": 1210,
+            "description": "Pure de papas cocidas a las brazas",
+            "image": "https://res.cloudinary.com/dbry8g6vk/image/upload/v1695230341/pure_de_papas_c9da7a12bf.png"
+        },
+        {
+            "id": 4,
+            "name": "Pure de calabazas",
+            "price": 900,
+            "description": "Pure de calabazas cocida a las brazas",
+            "image": "https://res.cloudinary.com/dbry8g6vk/image/upload/v1695230382/pure_de_calabaza_b34704e41c.png"
+        },
+        {
+            "id": 5,
+            "name": "Ensalada criolla",
+            "price": 1400,
+            "description": "Ensalada de Rúcula, radicheta, parmesano y tomates cherrys",
+            "image": "https://res.cloudinary.com/dbry8g6vk/image/upload/v1695230516/Ensalada_criolla_e1b25117f0.png"
+        },
+        {
+            "id": 6,
+            "name": "Pure mixto",
+            "price": 900,
+            "description": "Pure de calabaza y papa",
+            "image": "https://res.cloudinary.com/dbry8g6vk/image/upload/v1695230558/pure_mixto_c38301849f.png"
+        },
+        {
+            "id": 7,
+            "name": "Pimenton con huevo",
+            "price": 1200,
+            "description": "Morrones asados con huevo y especias",
+            "image": "https://res.cloudinary.com/dbry8g6vk/image/upload/v1695232326/small_morrones_asados_a9cc78cfd2.png"
+        },
+        {
+            "id": 8,
+            "name": "Verduras a las brasas",
+            "price": 1500,
+            "description": "Verduras asadas: Esparragos, hongos, pimientos rojo,verde y amarillo, pepino, berenjena",
+            "image": "https://res.cloudinary.com/dbry8g6vk/image/upload/v1695230636/verduras_a_las_brasas_df83bae3c0.png"
+        }
+    ],
+    "postres":[
+        {
+            "id": 1,
+            "name": "Flan",
+            "price": 680,
+            "description": "Flan casero con dulce de leche y crema",
+            "image": "https://res.cloudinary.com/dbry8g6vk/image/upload/v1695230713/Flan_Casero_1_aac7e491e3.png"
+        },
+        {
+            "id": 2,
+            "name": "Durazno y ciruelas Malbec a las brazas",
+            "price": 1000,
+            "description": "Coccion y reduccion de duraznos en vino Malbec a las brazas",
+            "image": "https://res.cloudinary.com/dbry8g6vk/image/upload/v1696764277/durazno_a_las_brazas_eeagxi.png"
+        },
+        {
+            "id": 3,
+            "name": "Ensalada de frutas",
+            "price": 680,
+            "description": "Vaso de 300 ml de ensalada de fruta,contiene frutilla,naranja,kiwi, banana, manzana, anana, durazno, cereza",
+            "image": "https://res.cloudinary.com/dbry8g6vk/image/upload/v1695230780/ensalada_frutas_4dd0114eff.png"
+        },
+        {
+            "id": 4,
+            "name": "Helados",
+            "price": 660,
+            "description": "Copa con 3 bochas de helado a eleccion",
+            "image": "https://res.cloudinary.com/dbry8g6vk/image/upload/v1695230833/helado_d1979e2914.png"
+        },
+        {
+            "id": 5,
+            "name": "Brownie con helado de crema",
+            "price": 880,
+            "description": "Browni adornado con bocha de Helado a eleccion",
+            "image": "https://res.cloudinary.com/dbry8g6vk/image/upload/v1695230882/americadigital_brownie_helado_3da7c60d94.png"
+        }
+    ],
+    
+}
--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-const express = require("express");
-
+import express from "express";
+import FAKE_DB from "./db";
 
 const app = express();
 
-
-app.get("/data", (req, res) => {
-  // Leer todos los datos desde tu archivo JSON y responder con ellos
-  const data = require("./db.json");
-  res.json(data);
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
 });
 
-app.listen(5000, () => {
-  console.log("Running on port 5000.");
+// Ruta para obtener todos los productos
+app.get("/api/products", (req, res) => {
+  res.json(FAKE_DB);
 });
+
+// Ruta para obtener productos por categoría
+app.get("/api/products/category/:category", (req, res) => {
+  const category = req.params.category;
+  const productsInCategory = FAKE_DB.filter((product) => product.category === category);
+  res.json(productsInCategory);
+});
+
 
 module.exports = app;
